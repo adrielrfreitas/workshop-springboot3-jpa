ca05688afaeebbcdd48cc6c32bf88e6b3221ef60
Adriel Rober Freitas Fernandes <adriel.roberfreitas@outlook.com>
2024-07-12 11:25:33 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/devfreitas/course/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.devfreitas.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devfreitas.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>
+{
+	
+}
