10c5c2e87277f06dd0075e5e41c7e384c72af4ba
Adriel Rober Freitas Fernandes <adriel.roberfreitas@outlook.com>
2024-07-16 10:14:06 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/devfreitas/couse/entities/Product.java
+++ src/main/java/com/devfreitas/couse/entities/Product.java
@@ -9,15 +9,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable
-{
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -25,17 +26,16 @@ public class Product implements Serializable
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	@Transient
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
-	public Product()
-	{
-		
+
+	public Product() {
+
 	}
 
-	public Product(Long id, String name, String description, Double price, String imgUrl) 
-	{
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -44,70 +44,57 @@ public class Product implements Serializable
 		this.imgUrl = imgUrl;
 	}
 
-	public Long getId() 
-	{
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Long id) 
-	{
+	public void setId(Long id) {
 		this.id = id;
 	}
 
-	public String getName() 
-	{
+	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) 
-	{
+	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String getDescription() 
-	{
+	public String getDescription() {
 		return description;
 	}
 
-	public void setDescription(String description) 
-	{
+	public void setDescription(String description) {
 		this.description = description;
 	}
 
-	public Double getPrice() 
-	{
+	public Double getPrice() {
 		return price;
 	}
 
-	public void setPrice(Double price) 
-	{
+	public void setPrice(Double price) {
 		this.price = price;
 	}
 
-	public String getImgUrl() 
-	{
+	public String getImgUrl() {
 		return imgUrl;
 	}
 
-	public void setImgUrl(String imgUrl) 
-	{
+	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
 	}
 
-	public Set<Category> getCategories() 
-	{
+	public Set<Category> getCategories() {
 		return categories;
 	}
 
 	@Override
-	public int hashCode() 
-	{
+	public int hashCode() {
 		return Objects.hash(id);
 	}
 
 	@Override
-	public boolean equals(Object obj) 
-	{
+	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
 		if (obj == null)
@@ -116,5 +103,5 @@ public class Product implements Serializable
 			return false;
 		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
-	}	
+	}
 }
