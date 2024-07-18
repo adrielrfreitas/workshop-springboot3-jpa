d9e4e6b4369c562b3bc1e279feaa9044453bb33c
Adriel Rober Freitas Fernandes <adriel.roberfreitas@outlook.com>
2024-07-18 09:52:38 -0300

    Exception handling - findById

--- src/main/java/com/devfreitas/course/services/UserService.java
+++ src/main/java/com/devfreitas/course/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devfreitas.course.repositories.UserRepository;
+import com.devfreitas.course.services.exceptions.ResourceNotFoundException;
 import com.devfreitas.couse.entities.User;
 
 @Service
@@ -23,7 +24,7 @@ public class UserService
 	public User findById(Long id)
 	{
 		Optional<User> obj = repository.findById(id);
-		return obj.get();	
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));	
 	}
 	
 	public User insert(User obj)
